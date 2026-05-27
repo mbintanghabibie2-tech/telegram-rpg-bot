71ac2cab6b7402ce24e08a1a6777e675214acef9
mbintanghabibie2-tech <mbintanghabibie2@gmail.com>
2026-05-27 15:43:46 +0700

    Update supabase.js

--- database/supabase.js
+++ database/supabase.js
@@ -1,12 +1,17 @@
 import { createClient }
 from '@supabase/supabase-js'
 
+import dotenv
+from 'dotenv'
+
+dotenv.config()
+
 const supabase =
 createClient(
 
-'https://eextshaeqrhaeevlosex.supabase.co/',
+process.env.SUPABASE_URL,
 
-'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVleHRzaGFlcXJoYWVldmxvc2V4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4NjM4MDQsImV4cCI6MjA5NTQzOTgwNH0.poPZA0dqC5lpEU2EgLE2F4OTvqoZNcHUv-2hpZ0vAxI'
+process.env.SUPABASE_KEY
 
 )
 
