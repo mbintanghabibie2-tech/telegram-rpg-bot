4b7a6783feae795791db390b5f923da909d052d6
mbintanghabibie2-tech <mbintanghabibie2@gmail.com>
2026-05-27 14:18:45 +0700

    start.js

--- commands/start.js
+++ commands/start.js
@@ -1,1 +1,28 @@
+import { InlineKeyboard }
+from 'grammy'
 
+export async function startCommand(
+ctx
+) {
+
+const keyboard =
+new InlineKeyboard()
+.text(
+'Daftar Sekarang',
+'register'
+)
+
+await ctx.reply(
+
+`Hei. disini kamu bisa memainkan game seru,
+
+daftar terlebih dahulu sebelum memainkannya!`,
+
+{
+reply_markup:
+keyboard
+}
+
+)
+
+}
