064a7459868a364c0c70d3127c42a937f30282d2
mbintanghabibie2-tech <mbintanghabibie2@gmail.com>
2026-05-27 18:30:49 +0700

    Update supabase.js

--- database/supabase.js
+++ database/supabase.js
@@ -1,18 +1,17 @@
-import { createClient }
-from '@supabase/supabase-js'
-
-import dotenv
-from 'dotenv'
+import { createClient } from '@supabase/supabase-js'
+import dotenv from 'dotenv'
+import ws from 'ws'
 
 dotenv.config()
 
-const supabase =
-createClient(
-
+const supabase = createClient(
 process.env.SUPABASE_URL,
-
-process.env.SUPABASE_KEY
-
+process.env.SUPABASE_KEY,
+{
+ realtime: {
+   transport: ws
+ }
+}
 )
 
 export default supabase
