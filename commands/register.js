f3f2467e1eaee0f396c9a1a654a4a632e75c8ef4
mbintanghabibie2-tech <mbintanghabibie2@gmail.com>
2026-05-27 15:11:51 +0700

    Update register.js

--- commands/register.js
+++ commands/register.js
@@ -1,6 +1,10 @@
+import supabase
+from '../database/supabase.js'
+
 const waitingRegister =
 new Set()
 
+// BUTTON REGISTER 😭🔥
 export async function registerCommand(
 ctx
 ) {
@@ -20,6 +24,107 @@ nama_kamu
 
 tanpa spasi`
 
+)
+
+}
+
+// BUAT PLAYER 😭🔥
+export async function createPlayer(
+
+ctx,
+nickname
+
+) {
+
+const userId =
+ctx.from.id
+
+const username =
+ctx.from.username
+? `@${ctx.from.username}`
+: '@ belum pasang username'
+
+// CEK SUDAH DAFTAR 😭🔥
+const {
+data:
+existing
+}
+=
+await supabase
+
+.from('players')
+
+.select('*')
+
+.eq(
+'telegram_id',
+userId
+)
+
+.single()
+
+if(
+existing
+) {
+
+return ctx.reply(
+
+'kamu sudah terdaftar'
+
+)
+
+}
+
+// CEK NICKNAME 😭🔥
+const {
+data:
+sameName
+}
+=
+await supabase
+
+.from('players')
+
+.select('*')
+
+.eq(
+'nickname',
+nickname
+)
+
+.single()
+
+if(
+sameName
+) {
+
+return ctx.reply(
+
+'nickname sudah dipakai'
+
+)
+
+}
+
+// INSERT PLAYER 😭🔥
+await supabase
+
+.from('players')
+
+.insert({
+
+telegram_id:
+userId,
+
+nickname,
+
+username
+
+})
+
+// HAPUS DARI WAITING 😭🔥
+waitingRegister.delete(
+userId
 )
 
 }
